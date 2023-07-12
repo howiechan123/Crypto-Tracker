3eb9cb014d66951a1b6ff858d84bc8d37edb1605
howiechan123 <113634531+howiechan123@users.noreply.github.com>
2023-07-12 16:52:07 -0400

    added project

---
+++ cryptotracker/project/coins/tasks.py
@@ -0,0 +1,42 @@
+import requests
+from django.forms.models import model_to_dict
+from celery import shared_task
+from .models import Coin
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
+channel_layer = get_channel_layer()
+
+
+@shared_task
+def get_coins_data():
+    url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
+    data = requests.get(url).json()
+    
+    coins = []
+    
+    for coin in data:
+        obj, created = Coin.objects.get_or_create(symbol=coin['symbol'])
+        
+        obj.name = coin['name']
+        obj.symbol = coin['symbol']
+        
+        if(obj.price > coin['current_price']):
+            state = 'down'
+        elif(obj.price < coin['current_price']):
+            state = 'up'
+        else:
+            state = 'same'
+        
+        obj.price = coin['current_price']
+        obj.rank = coin['market_cap_rank']
+        obj.image = coin['image']
+        
+        obj.save()
+        
+        new_data = model_to_dict(obj)
+        new_data.update({'state':state})
+        
+        coins.append(new_data)
+    
+    async_to_sync(channel_layer.group_send)('coins', {'type': 'send_new_data', 'text': coins})
